0b4297099f44fe6eb9e8cac87d46879480dfc2c1
AbbasSaad27 <xenozb@gmail.com>
2022-04-21 12:42:48 +0600

    reimagined the template a bit

--- js/app.js
+++ js/app.js
@@ -71,9 +71,15 @@ const getCommentsData = async function() {
                         </div>`
 
                         if(replies[0]) {
+                            commentHtml += `<div class="all-replies"></div>`
+                            commentSection.insertAdjacentHTML("beforeend", commentHtml)
+
+                            const allReplies = document.querySelector(".all-replies")
+
+                            let repliesHtml = ``
                             replies.forEach(function(reply) {
                                 console.log(reply)
-                                commentHtml += `<div class="comment-n-reply-box reply-block">
+                                repliesHtml += `<div class="comment-n-reply-box reply-block">
                                 <article class="comment-box">
                                 <div class="votes">
                                     <button type="button" class="btn btn-vote btn-upvote">
@@ -81,7 +87,7 @@ const getCommentsData = async function() {
                                         <path class="icon icon-up" d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" fill="#C5C6EF"/>
                                     </svg>
                                     </button>
-                                    <span class="count-vote">${score}</span>
+                                    <span class="count-vote">${reply.score}</span>
                                     <button type="button" class="btn btn-vote btn-downvote">
                                     <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg">
                                         <path class="icon icon-down" d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" fill="#C5C6EF"/>
@@ -91,12 +97,12 @@ const getCommentsData = async function() {
                                 <div class="about-comment">
                                     <div>
                                     <div class="about-user">
-                                        <img class="user-pfp" src="${user.image.png}" alt="">
-                                        <p class="name">${user.username}</p>
-                                        <span class="createdAt">${createdAt}</span>
+                                        <img class="user-pfp" src="${reply.user.image.png}" alt="">
+                                        <p class="name">${reply.user.username}</p>
+                                        <span class="createdAt">${reply.createdAt}</span>
                                     </div>
                                     <div class="btns-container">
-                                    ${user.username === currentuserName ? 
+                                    ${reply.user.username === currentuserName ? 
                                         `<button type="button" class="btn btn-del">
                                             <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
                                                 <path class="icon icon-del" d="M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z" fill="#ED6368"/>
@@ -120,10 +126,10 @@ const getCommentsData = async function() {
                                     }
                                     </div>
                                     </div>
-                                    <p class="comment">${content}</p>
+                                    <p class="comment">${reply.content}</p>
                                 </div>
                                 </article>
-                                ${user.username !== currentuserName && `<div class="reply-box hidden">
+                                ${reply.user.username !== currentuserName && `<div class="reply-box hidden">
                                 <img class="user-pfp" src="images/avatars/image-amyrobson.png" alt="user">
                                 <form class="reply-form">
                                     <textarea class="reply-textbox" name="reply" id="reply"></textarea>
@@ -134,11 +140,12 @@ const getCommentsData = async function() {
                                 </div>`}
                             </div>`
                             })
+
+                            allReplies.insertAdjacentHTML("afterbegin", repliesHtml)
+                            return;
                         }
                         commentSection.insertAdjacentHTML("beforeend", commentHtml)
     })
-
-    
 }
 
 getCommentsData()
